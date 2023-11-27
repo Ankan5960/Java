bfc84a0cd4b034293a34ab44680dad9a01a40b61
Ankan5960 <104480693+Ankan5960@users.noreply.github.com>
2023-11-27 14:07:34 +0530

    first java program

--- hello.java
+++ hello.java
@@ -1,5 +1,12 @@
-class Hello{
-    public static void main(String[] args) {
+class H{
+    public void Display(){
         System.out.println("hello world");
+    }
+} 
+
+class Main{
+    public static void main(String[] args) {
+        H ankan=new H();
+        ankan.Display();
     }
 }
